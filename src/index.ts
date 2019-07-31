c31d4c1c11fb55db7a9a716e60d6c56dec4b9bab
markinspencer <markinspencer@gmail.com>
2019-07-31 18:50:15 -0400

    add initial user model

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hi There");
+import { User } from "./models/User";
+
+const user = new User({ name: "Spencer", age: 28 });
+
+user.set({ age: 29 });
+console.log(user);
+user.set({ name: "Mai", age: 24 });
+console.log(user);
