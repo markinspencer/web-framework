b5edceb7fe0e150bc763b0831709070ecaa3d943
markinspencer <markinspencer@gmail.com>
2019-08-01 19:48:57 -0400

    add fetch and save functionality to User

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "Spencer", age: 28 });
-
-user.on("change", () => {
-  console.log("change 1");
-});
-
-user.on("change", () => {
-  console.log("change 2");
-});
-
-user.on("save", () => {
-  console.log("save was triggered");
-});
-
-user.trigger("change");
-user.trigger("save");
-user.trigger("doesntExist");
+const user = new User({ name: "newrecord", age: 0 });
+user.save();
