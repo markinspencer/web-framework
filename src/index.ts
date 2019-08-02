501ab314d394b040188092e0ba2c6d9d8bee3750
Spencer Markin <smarkin@questica.com>
2019-08-02 16:20:42 -0400

    reimplement functionality on User

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "newrecord", age: 0 });
+const user = new User({ id: 1 });
 
-user.events.on("change", () => {
-  console.log("change made!");
+user.on("change", () => {
+  console.log(user);
 });
 
-user.events.trigger("change");
+user.fetch();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { AxiosResponse, AxiosError } from "axios";
 import { Attributes } from "./Attributes";
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
@@ -8,14 +9,55 @@ export interface UserProps {
   age?: number;
 }
 
-const rootUrl = "https://localhost:3000/users";
+const rootUrl = "http://localhost:3000/users";
 
 export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attributes: Attributes<UserProps>;
+  private events: Eventing = new Eventing();
+  private sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  private attributes: Attributes<UserProps>;
 
   constructor(attrs: UserProps) {
     this.attributes = new Attributes<UserProps>(attrs);
+  }
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.events.trigger("change");
+  }
+
+  fetch(): void {
+    const id = this.attributes.get("id");
+
+    if (typeof id !== "number") {
+      throw new Error("Cannot fetch without an id");
+    }
+
+    this.sync.fetch(id).then(
+      (response: AxiosResponse): void => {
+        this.set(response.data);
+      }
+    );
+  }
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse) => {
+        this.trigger("save");
+      })
+      .catch((err: AxiosError) => {
+        this.trigger("error");
+      });
   }
 }
